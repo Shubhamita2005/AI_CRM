00f00af0df47f88753167754cd38d5be57514499
gitsparthiv <ghoshparthiv98@gmail.com>
2026-08-04 02:12:16 +0530

    changes in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const dashboardRoutes = require("./routes/dashboard.routes");
 
 const app = express();
 
@@ -7,5 +8,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Backend is running!");
 });
+
+app.use("/api/dashboard", dashboardRoutes);
 
 module.exports = app;
