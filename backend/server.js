d08f7b92e554ec9a6997cf3206778e7fdc5acede
gitsparthiv <ghoshparthiv98@gmail.com>
2026-08-07 15:05:38 +0530

    server.js edited

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,6 @@ const app = require("./src/app");
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {//THIS is where requests start entering. Starts HTTP server on PORT
-        console.log(`Server running on http://localhost:${PORT}`);
+       console.log(`Server running on port ${PORT}`);
       });
       
