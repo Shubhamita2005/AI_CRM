b9dafcaa151680aae2266b2fed8545160f79417b
gitsparthiv <ghoshparthiv98@gmail.com>
2026-08-11 02:26:47 +0530

    add gmail api permission

--- backend/src/routes/google_auth_routes.js
+++ backend/src/routes/google_auth_routes.js
@@ -11,6 +11,7 @@ router.get("/google", (req, res) => {
         access_type: "offline",
         scope: [
             "https://www.googleapis.com/auth/calendar.events"
+            "https://www.googleapis.com/auth/gmail.send"
         ],
         prompt: "consent"
     });
